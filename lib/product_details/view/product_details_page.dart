28207fffe79e92ec7b2ff6bb62b8bb993d93f6c2
chitkoo <chitkokooo196@gmail.com>
2023-11-29 16:01:51 +0630

    update ui

--- lib/product_details/view/product_details_page.dart
+++ lib/product_details/view/product_details_page.dart
@@ -40,91 +40,98 @@ class _ProductDetailsView extends StatelessWidget {
 
     return Scaffold(
       backgroundColor: Colors.white,
-      body: CustomScrollView(
-        slivers: [
-          const SliverAppBar(
-            expandedHeight: kToolbarHeight,
-            // pinned: true,
-            backgroundColor: Colors.transparent,
-            systemOverlayStyle: SystemUiOverlayStyle(
-              statusBarBrightness: Brightness.dark,
-              statusBarIconBrightness: Brightness.dark,
+      appBar: AppBar(
+        actions: const [],
+      ),
+      body: ListView(
+        padding: EdgeInsets.fromLTRB(5.w, 0, 5.w, 2.5.w),
+        children: [
+          /**
+           * Carousel
+           */
+          FlutterCarousel(
+            options: CarouselOptions(
+              height: 300,
+              autoPlay: false,
+              viewportFraction: 1,
+              slideIndicator: CircularWaveSlideIndicator(
+                currentIndicatorColor: primary,
+                indicatorBackgroundColor: Colors.grey.shade300,
+              ),
+              floatingIndicator: true,
+            ),
+            items: sliders,
+          ),
+          SizedBox(
+            height: 3.w,
+          ),
+          Text(
+            '${product.title}',
+            style: const TextStyle(fontWeight: FontWeight.bold),
+          ),
+          const SizedBox(
+            height: 5,
+          ),
+          Text.rich(
+            TextSpan(
+              text: '\$ ',
+              children: [
+                TextSpan(
+                    text: '${product.price}',
+                    style: const TextStyle(color: Colors.black54)),
+              ],
+              style: const TextStyle(
+                color: Colors.black,
+                fontWeight: FontWeight.bold,
+              ),
             ),
           ),
-          SliverFillViewport(
-            delegate: SliverChildBuilderDelegate(
-              (context, index) {
-                return ListView(
-                  padding: EdgeInsets.fromLTRB(2.5.w, 0, 2.5.w, 2.5.w),
-                  children: [
-                    /**
-                     * Carousel
-                     */
-                    FlutterCarousel(
-                      options: CarouselOptions(
-                        height: 300,
-                        autoPlay: false,
-                        viewportFraction: 1,
-                        slideIndicator: CircularWaveSlideIndicator(
-                          currentIndicatorColor: primary,
-                          indicatorBackgroundColor: Colors.grey.shade300,
-                        ),
-                        floatingIndicator: true,
-                      ),
-                      items: sliders,
-                    ),
-                    SizedBox(
-                      height: 3.w,
-                    ),
-                    Text(
-                      '${product.title}',
-                      style: const TextStyle(fontWeight: FontWeight.bold),
-                    ),
-                    const SizedBox(
-                      height: 5,
-                    ),
-                    Text.rich(
-                      TextSpan(
-                        text: '\$ ',
-                        children: [
-                          TextSpan(
-                              text: '${product.price}',
-                              style: const TextStyle(color: Colors.black54)),
-                        ],
-                        style: const TextStyle(
-                          color: Colors.black,
-                          fontWeight: FontWeight.bold,
-                        ),
-                      ),
-                    ),
-                    SizedBox(
-                      height: 3.w,
-                    ),
-                    const Text(
-                      'Product Description',
-                      style: TextStyle(fontWeight: FontWeight.bold),
-                    ),
-                    const SizedBox(
-                      height: 5,
-                    ),
-                    Text('${product.description}'),
-                  ],
-                );
-              },
-            ),
+          SizedBox(
+            height: 3.w,
           ),
+          const Text(
+            'Product Description',
+            style: TextStyle(fontWeight: FontWeight.bold),
+          ),
+          const SizedBox(
+            height: 5,
+          ),
+          Text('${product.description}'),
         ],
       ),
       bottomNavigationBar: BottomAppBar(
         color: Colors.white,
         surfaceTintColor: Colors.transparent,
         height: 60,
-        padding: const EdgeInsets.all(5),
+        padding: EdgeInsets.symmetric(horizontal: 5.w, vertical: 5),
         child: Row(
           mainAxisAlignment: MainAxisAlignment.spaceBetween,
           children: [
-            Text('\$ ${product.price}'),
-            ElevatedButton(onPressed: () {}, child: const Text('Add to cart')),
+            Text(
+              'Price : \$ ${product.price}',
+              style: const TextStyle(
+                fontWeight: FontWeight.bold,
+                color: Colors.black,
+              ),
+            ),
+            // Text.rich(
+            //   TextSpan(
+            //     text: 'Price : ',
+            //     children: [
+            //       TextSpan(text: '\$ ${product.price}', sty),
+            //     ],
+            //     style: const TextStyle(
+            //       fontWeight: FontWeight.bold,
+            //       color: Colors.black,
+            //     ),
+            //   ),
+            // ),
+            SizedBox(
+              width: 20.w,
+            ),
+            Expanded(
+                child: ElevatedButton(
+                    onPressed: () {}, child: const Text('Add to Cart'))),
           ],
         ),
       ),
