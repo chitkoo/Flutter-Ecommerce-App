42dea56e924c1fb504f2372cf5b668813cc4cebc
chitkoo <chitkokooo196@gmail.com>
2023-11-29 21:14:05 +0630

    added favoruite ui

--- lib/product_details/view/product_details_page.dart
+++ lib/product_details/view/product_details_page.dart
@@ -1,6 +1,7 @@
+import 'package:fluentui_system_icons/fluentui_system_icons.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter/services.dart';
 import 'package:flutter_carousel_widget/flutter_carousel_widget.dart';
+import 'package:like_button/like_button.dart';
 import 'package:responsive_sizer/responsive_sizer.dart';
 
 import '../../models/product.dart';
@@ -41,62 +42,126 @@ class _ProductDetailsView extends StatelessWidget {
     return Scaffold(
       backgroundColor: Colors.white,
       appBar: AppBar(
-        actions: const [],
+        actions: [
+          Padding(
+            padding: const EdgeInsets.only(right: 30),
+            child: IconButton(
+              onPressed: () {},
+              icon: const Icon(FluentIcons.cart_24_regular),
+            ),
+          ),
+        ],
       ),
       body: ListView(
-        padding: EdgeInsets.fromLTRB(5.w, 0, 5.w, 2.5.w),
+        padding: EdgeInsets.zero,
+        shrinkWrap: true,
         children: [
           /**
            * Carousel
            */
-          FlutterCarousel(
-            options: CarouselOptions(
-              height: 300,
-              autoPlay: false,
-              viewportFraction: 1,
-              slideIndicator: CircularWaveSlideIndicator(
-                currentIndicatorColor: primary,
-                indicatorBackgroundColor: Colors.grey.shade300,
+          Stack(
+            children: [
+              FlutterCarousel(
+                options: CarouselOptions(
+                  height: 300,
+                  autoPlay: false,
+                  viewportFraction: 1,
+                  indicatorMargin: 0,
+                  slideIndicator: CircularWaveSlideIndicator(
+                    currentIndicatorColor: primary,
+                    indicatorBackgroundColor: Colors.grey.shade300,
+                    indicatorRadius: 4,
+                    itemSpacing: 12,
+                  ),
+                  floatingIndicator: true,
+                ),
+                items: sliders,
               ),
-              floatingIndicator: true,
-            ),
-            items: sliders,
+              Positioned(
+                right: 35,
+                bottom: 0,
+                child: LikeButton(
+                  padding: EdgeInsets.zero,
+                  // onTap: (bool isLike) async {
+                  //   return null;
+                  // },
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  size: 30,
+                  circleColor: const CircleColor(
+                      start: Color(0xffE81031), end: Color(0xffE81031)),
+                  bubblesColor: const BubblesColor(
+                    dotPrimaryColor: Color(0xffB70011),
+                    dotSecondaryColor: Color(0xffE81031),
+                  ),
+                  likeBuilder: (bool isLiked) {
+                    return Container(
+                      width: 30,
+                      height: 30,
+                      decoration: BoxDecoration(
+                          shape: BoxShape.circle,
+                          color: Colors.white,
+                          boxShadow: [
+                            BoxShadow(
+                              color: primary.withOpacity(0.2),
+                              offset: const Offset(1, 1),
+                              blurRadius: 1,
+                              spreadRadius: 1,
+                            )
+                          ]),
+                      child: Icon(
+                        isLiked
+                            ? FluentIcons.heart_48_filled
+                            : FluentIcons.heart_48_regular,
+                        color: isLiked ? Colors.redAccent : Colors.grey,
+                        size: 20,
+                      ),
+                    );
+                  },
+                ),
+              ),
+            ],
           ),
-          SizedBox(
-            height: 3.w,
-          ),
-          Text(
-            '${product.title}',
-            style: const TextStyle(fontWeight: FontWeight.bold),
-          ),
-          const SizedBox(
-            height: 5,
-          ),
-          Text.rich(
-            TextSpan(
-              text: '\$ ',
+          Padding(
+            padding: EdgeInsets.symmetric(horizontal: 5.w, vertical: 3.w),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
               children: [
-                TextSpan(
-                    text: '${product.price}',
-                    style: const TextStyle(color: Colors.black54)),
+                Text(
+                  '${product.title}'.toUpperCase(),
+                  style: const TextStyle(fontWeight: FontWeight.bold),
+                ),
+                const SizedBox(
+                  height: 5,
+                ),
+                Text.rich(
+                  TextSpan(
+                    text: '\$ ',
+                    children: [
+                      TextSpan(
+                          text: '${product.price}',
+                          style: const TextStyle(color: Colors.black54)),
+                    ],
+                    style: const TextStyle(
+                      color: Colors.black,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                ),
+                Divider(
+                  color: Colors.black.withOpacity(0.1),
+                ),
+                Text(
+                  'Product Description'.toUpperCase(),
+                  style: const TextStyle(fontWeight: FontWeight.bold),
+                ),
+                const SizedBox(
+                  height: 5,
+                ),
+                Text('${product.description}'),
               ],
-              style: const TextStyle(
-                color: Colors.black,
-                fontWeight: FontWeight.bold,
-              ),
             ),
           ),
-          SizedBox(
-            height: 3.w,
-          ),
-          const Text(
-            'Product Description',
-            style: TextStyle(fontWeight: FontWeight.bold),
-          ),
-          const SizedBox(
-            height: 5,
-          ),
-          Text('${product.description}'),
         ],
       ),
       bottomNavigationBar: BottomAppBar(
@@ -114,18 +179,6 @@ class _ProductDetailsView extends StatelessWidget {
                 color: Colors.black,
               ),
             ),
-            // Text.rich(
-            //   TextSpan(
-            //     text: 'Price : ',
-            //     children: [
-            //       TextSpan(text: '\$ ${product.price}', sty),
-            //     ],
-            //     style: const TextStyle(
-            //       fontWeight: FontWeight.bold,
-            //       color: Colors.black,
-            //     ),
-            //   ),
-            // ),
             SizedBox(
               width: 20.w,
             ),
