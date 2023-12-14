6a675c3b22c73dd512a9ca68de8e92779e52b000
chitkoo <chitkokooo196@gmail.com>
2023-12-14 21:10:13 +0630

    updated home screen

--- lib/shop/view/shop_page.dart
+++ lib/shop/view/shop_page.dart
@@ -10,108 +10,56 @@ import '../../common/common_methods.dart';
 import '../../search/search.dart';
 import '../../shopping_bag/shopping_bag.dart';
 import '../../utils/colors.dart';
+import '../../utils/constants.dart';
 
 class ShopPage extends StatelessWidget {
   const ShopPage({super.key});
 
   @override
   Widget build(BuildContext context) {
-    const categories = [
-      "smartphones",
-      "laptops",
-      "fragrances",
-      "skincare",
-      "groceries",
-      "home-decoration",
-      "furniture",
-      "tops",
-      "womens-dresses",
-      "womens-shoes",
-      "mens-shirts",
-      "mens-shoes",
-      "mens-watches",
-      "womens-watches",
-      "womens-bags",
-      "womens-jewellery",
-      "sunglasses",
-      "automotive",
-      "motorcycle",
-      "lighting"
-    ];
-
     return Scaffold(
-      ///`Custom App Bar`
-      appBar: const HomeAppBar(),
+      appBar: const _HomeAppBar(),
       body: CustomScrollView(
         slivers: [
-          const CustomMargin(),
-
           ///`Categories Header`
-          HeaderSection(
+          _HeaderSection(
             headerTitle: 'Trending Categories',
             onTap: () {},
           ),
-          const CustomMargin(),
 
           ///`Categories List`
-          const CategoriesList(categories: categories),
-          const CustomMargin(),
+          const _CategoriesList(categories: categories),
 
           ///`Smartphones Header`
-          HeaderSection(
+          _HeaderSection(
             headerTitle: 'Popular Smartphones',
             onTap: () {},
           ),
-          const CustomMargin(),
 
           ///`Smartphones List`
-          const SmartphonesList(),
-          const CustomMargin(),
+          const _PopularList(),
 
           ///`Laptops Header`
-          HeaderSection(
+          _HeaderSection(
             headerTitle: 'Popular Laptops',
             onTap: () {},
           ),
-          const CustomMargin(),
 
           ///`Laptops List`
-          const SmartphonesList(),
-          const CustomMargin(),
+          const _PopularList(),
+          SliverToBoxAdapter(
+            child: SizedBox(
+              height: 5.w,
+            ),
+          ),
         ],
       ),
     );
   }
 }
 
-class SmartphonesList extends StatelessWidget {
-  const SmartphonesList({
-    super.key,
-  });
-
-  @override
-  Widget build(BuildContext context) {
-    return SliverToBoxAdapter(
-      child: SizedBox(
-        height: 55.w,
-        child: ListView.builder(
-          addAutomaticKeepAlives: false,
-          addRepaintBoundaries: false,
-          scrollDirection: Axis.horizontal,
-          itemCount: 5,
-          itemBuilder: (BuildContext context, int index) {
-            return const ProductCard();
-          },
-        ),
-      ),
-    );
-  }
-}
-
-class HomeAppBar extends StatelessWidget implements PreferredSizeWidget {
-  const HomeAppBar({
-    super.key,
-  });
+class _HomeAppBar extends StatelessWidget implements PreferredSizeWidget {
+  const _HomeAppBar();
 
   @override
   Widget build(BuildContext context) {
@@ -190,6 +138,113 @@ class HomeAppBar extends StatelessWidget implements PreferredSizeWidget {
 
   @override
   Size get preferredSize => const Size.fromHeight(120);
+}
+
+class _HeaderSection extends StatelessWidget {
+  const _HeaderSection({
+    required this.headerTitle,
+    required this.onTap,
+  });
+
+  final String headerTitle;
+  final VoidCallback onTap;
+
+  @override
+  Widget build(BuildContext context) {
+    return SliverToBoxAdapter(
+      child: Container(
+        padding: EdgeInsets.symmetric(horizontal: 2.5.w),
+        margin: EdgeInsets.only(top: 5.w, bottom: 3.w),
+        width: 100.w,
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [
+            Text(
+              headerTitle,
+              style: Theme.of(context).textTheme.titleMedium,
+            ),
+            InkWell(
+              onTap: () {},
+              child: Text(
+                'See all',
+                style: Theme.of(context).textTheme.bodyMedium?.copyWith(
+                      color: primary,
+                      fontWeight: FontWeight.bold,
+                    ),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class _CategoriesList extends StatelessWidget {
+  const _CategoriesList({
+    required this.categories,
+  });
+
+  final List<String> categories;
+
+  @override
+  Widget build(BuildContext context) {
+    return SliverToBoxAdapter(
+      child: SizedBox(
+        height: 10.w,
+        child: ListView.builder(
+          addAutomaticKeepAlives: false,
+          addRepaintBoundaries: false,
+          scrollDirection: Axis.horizontal,
+          itemCount: categories.length,
+          itemBuilder: (BuildContext context, int index) {
+            final categoryItem = categories[index];
+
+            return Container(
+              key: ValueKey(categoryItem),
+              padding: EdgeInsets.symmetric(horizontal: 2.w),
+              alignment: Alignment.center,
+              margin: EdgeInsets.symmetric(horizontal: 2.5.w),
+              decoration: BoxDecoration(
+                color: Colors.white,
+                borderRadius: BorderRadius.circular(2.w),
+                boxShadow: [
+                  BoxShadow(color: Colors.black.withOpacity(0.1)),
+                ],
+              ),
+              clipBehavior: Clip.antiAlias,
+              child: InkWell(
+                onTap: () {},
+                child: Text(categoryItem.capitalizeString),
+              ),
+            );
+          },
+        ),
+      ),
+    );
+  }
+}
+
+class _PopularList extends StatelessWidget {
+  const _PopularList();
+
+  @override
+  Widget build(BuildContext context) {
+    return SliverToBoxAdapter(
+      child: SizedBox(
+        height: 55.w,
+        child: ListView.builder(
+          addAutomaticKeepAlives: false,
+          addRepaintBoundaries: false,
+          scrollDirection: Axis.horizontal,
+          itemCount: 5,
+          itemBuilder: (BuildContext context, int index) {
+            return const ProductCard();
+          },
+        ),
+      ),
+    );
+  }
 }
 
 class ProductCard extends StatelessWidget {
@@ -292,101 +347,6 @@ class ProductCard extends StatelessWidget {
             ),
           ),
         ],
-      ),
-    );
-  }
-}
-
-class CustomMargin extends StatelessWidget {
-  const CustomMargin({
-    super.key,
-  });
-
-  @override
-  Widget build(BuildContext context) {
-    return SliverPadding(padding: EdgeInsets.only(bottom: 5.w));
-  }
-}
-
-class CategoriesList extends StatelessWidget {
-  const CategoriesList({
-    super.key,
-    required this.categories,
-  });
-
-  final List<String> categories;
-
-  @override
-  Widget build(BuildContext context) {
-    return SliverToBoxAdapter(
-      child: SizedBox(
-        height: 10.w,
-        child: ListView.builder(
-          addAutomaticKeepAlives: false,
-          addRepaintBoundaries: false,
-          scrollDirection: Axis.horizontal,
-          itemCount: categories.length,
-          itemBuilder: (BuildContext context, int index) {
-            final categoryItem = categories[index];
-
-            return Container(
-              padding: EdgeInsets.symmetric(horizontal: 2.w),
-              height: 10.w,
-              alignment: Alignment.center,
-              margin: EdgeInsets.symmetric(horizontal: 2.5.w),
-              decoration: BoxDecoration(
-                color: Colors.white,
-                borderRadius: BorderRadius.circular(2.w),
-                boxShadow: [
-                  BoxShadow(color: Colors.black.withOpacity(0.1)),
-                ],
-              ),
-              clipBehavior: Clip.antiAlias,
-              child: InkWell(
-                  onTap: () {}, child: Text(categoryItem.capitalizeString)),
-            );
-          },
-        ),
-      ),
-    );
-  }
-}
-
-class HeaderSection extends StatelessWidget {
-  const HeaderSection({
-    super.key,
-    required this.headerTitle,
-    required this.onTap,
-  });
-
-  final String headerTitle;
-  final VoidCallback onTap;
-
-  @override
-  Widget build(BuildContext context) {
-    return SliverToBoxAdapter(
-      child: Container(
-        padding: EdgeInsets.symmetric(horizontal: 2.5.w),
-        width: 100.w,
-        child: Row(
-          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-          children: [
-            Text(
-              headerTitle,
-              style: Theme.of(context).textTheme.titleMedium,
-            ),
-            InkWell(
-              onTap: () {},
-              child: Text(
-                'See all',
-                style: Theme.of(context).textTheme.bodyMedium?.copyWith(
-                      color: primary,
-                      fontWeight: FontWeight.bold,
-                    ),
-              ),
-            ),
-          ],
-        ),
       ),
     );
   }
